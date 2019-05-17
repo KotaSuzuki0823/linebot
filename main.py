bcc5fb426cc799d98958fb91e30ae1f97a083880
kotasuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2019-05-18 01:44:05 +0900

    フラグの削除

--- main.py
+++ main.py
@@ -66,32 +66,23 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     getMesege = event.message.text;# ユーザが送信したメッセージ(event.message.text)を取得
-    keyword = ['なにこれ','祭り','ヘルプ','今は昼だよね','真夜中のカーニバル']
+    keyword = ['なにこれ','祭り','ヘルプ',]
+    imgKeyWord ['の画像']
 
     if getMesege not in keyword:
-        if flag == 0:
-            message = getMesege + 'だあああああぁぁぁぁぁ'#通常モードはオウム返し
-        elif flag == 1:
-            flag = 2
-            SEARCH_TERM = getMesege
+        message = getMesege + 'だあああああぁぁぁぁぁ'#通常モードはオウム返し
 
     elif getMesege == 'なにこれ':#キーワードでモード変更
         message = '私はまつりちゃん．お祭り大好き！「お祭り」と打つとお祭りを始めるよ！私が何のお祭りやるか聞くから，やりたいお祭りを打ってね！\nお祭りって言っても，ただBing画像検索で検索して出てくる画像を適当に選んで君に送信するだけだけどね(笑)'
 
     elif getMesege == '祭り':
         message = 'なんの祭り????'
-        flag = 1
 
     elif getMesege == 'ヘルプ':
         message = '「なにこれ」：このBOTの説明をするよ\n「祭り」：祭りを始めるよ\n「ヘルプ」：これ\n「今は昼だね」：セーフサーチオン\n'
 
-    elif getMesege == '今は昼だよね':
-        message = 'セーフサーチONだね'
-        safeSerchFlag = "ON"
-
-    elif getMesege == '真夜中のカーニバル':
-        message = 'セーフサーチOFFにしたよ'
-        safeSerchFlag = "OFF"
+    elif getMesege in imgKeyWord:
+        message = 'ここに画像が来る予定です．'
 
     line_bot_api.reply_message(
         event.reply_token,
@@ -101,10 +92,6 @@ def handle_message(event):
 
 # ポート番号の設定
 if __name__ == "__main__":
-    #モードフラグ
-    #通常時：0，お祭り待機状態：1　お祭り状態：２
-    flag = 0
-    safeSerchFlag = "ON"
     #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
